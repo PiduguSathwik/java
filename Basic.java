cbff9882cc0110418130e5ae0f566ab2fd204c6b
Pidugu Sathwik Kumar Reddy <165761619+PiduguSathwik@users.noreply.github.com>
2025-02-10 12:40:17 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,32 @@
+//Day 1
+class Basic {
+    public static void main(String args[]) {
+         
+        /*int a=5;
+        int b=9;
+
+        int sum =a+b;
+        int diff =a-b;
+        int mul = a*b;
+        int div =a/b;//integer Division(Quotient)
+        int rem=a%b;//Remainder
+
+        System.out.println("Sum of a + b  = " + sum);
+        System.out.println("Diff of a - b = " + diff);
+        System.out.println("Mul of a * b = " + mul );
+        System.out.println("Div of a / b = " + div);
+        System.out.println("Rem of a % b = " + rem);*/
+
+
+        //CONDITION STATEMENTS
+
+        boolean IsRaining =true;
+        if(IsRaining){
+            System.out.println("Raining");
+        }
+        if(IsRaining){
+            System.out.println("Not Raining");
+        }
+
+    }
+}
